b053ac309c2372c1c2c7447c89a9efae231d42ff
hanfeng <HF@8844028.com>
2018-08-03 03:06:46 -0700

    状态机测试

---
+++ src/robot/robot_test.go
@@ -0,0 +1,121 @@
+package robot
+
+import (
+	//	"common"
+	//	"fmt"
+	"fsm"
+	"logger"
+	//	"net"
+	"rpc"
+	//	"runtime/debug"
+)
+
+type SRobot struct {
+	uid             uint64
+	serverForClient *rpc.Server
+	stateMatchine   *fsm.FSM
+}
+
+var robot *SRobot
+
+const (
+	addr = "127.0.0.1:7900"
+)
+
+func CreateRobot() *SRobot {
+	robot := &SRobot{}
+	robot.stateMatchine = fsm.CreateFSM()
+
+	key1 := "connectserver"
+	matchine1 := fsm.CreateMatchineState(key1, nil, key1, robot, "ConnectGameServer")
+	robot.stateMatchine.AddState(key1, matchine1)
+	robot.stateMatchine.SetDefaultState(matchine1)
+
+	key2 := "pinggame"
+	matchine2 := fsm.CreateMatchineState(key2, nil, key2, robot, "SendPing")
+	robot.stateMatchine.AddState(key1, matchine2)
+
+	robot.stateMatchine.Start()
+	return robot
+}
+func (self *SRobot) ConnectGameServer(key string) {
+	logger.Info("ConnectGameServer")
+}
+func (self *SRobot) SendPing(key string) {
+	logger.Info("SendPing")
+}
+
+/*
+func (self *SRobot) ConnectGameServer(key string) {
+	logger.Info("ConnectGameServer")
+
+	lRpcServer := rpc.NewServer()
+	self.serverForClient = lRpcServer
+	lRpcServer.Register(self)
+
+	lRpcServer.RegCallBackOnConn(
+		func(conn rpc.RpcConn) {
+			self.onConn(conn)
+		},
+	)
+
+	lRpcServer.RegCallBackOnDisConn(
+		func(conn rpc.RpcConn) {
+			self.onDisConn(conn)
+		},
+	)
+
+	lRpcServer.RegCallBackOnCallBefore(
+		func(conn rpc.RpcConn) {
+			conn.Lock()
+		},
+	)
+
+	lRpcServer.RegCallBackOnCallAfter(
+		func(conn rpc.RpcConn) {
+			conn.Unlock()
+		},
+	)
+
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		fmt.Println("连接服务端失败:", err.Error())
+		return
+	}
+
+	go func() {
+		rpcConn := rpc.NewProtoBufConn(lRpcServer, conn, 128, 45)
+		defer func() {
+			if r := recover(); r != nil {
+				logger.Error("player rpc runtime error begin:", r)
+				debug.PrintStack()
+				self.onDisConn(rpcConn)
+				rpcConn.Close()
+
+				logger.Error("player rpc runtime error end ")
+			}
+		}()
+		lRpcServer.ServeConn(rpcConn)
+	}()
+
+}
+
+func (c *SRobot) onConn(conn rpc.RpcConn) {
+	logger.Info("onConn")
+}
+
+func (self *SRobot) onDisConn(conn rpc.RpcConn) {
+	logger.Info("onDisConn")
+}
+
+func SendLoginMsg(conn rpc.RpcConn) {
+	logger.Info("SendLoginMsg")
+
+	return
+}
+
+func SendMsg(conn rpc.RpcConn, value interface{}) {
+	logger.Info("SendMsg")
+	common.WriteClientResult(conn, "CNServer.Login", value)
+}
+*/
