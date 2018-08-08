c8924aa01bed166a764bb9405333976821fddc16
hanfeng <HF@8844028.com>
2018-08-08 00:39:56 -0700

    机器人连接大厅接口

--- src/robot/robotRecvMsg.go
+++ src/robot/robotRecvMsg.go
@@ -1,1 +1,12 @@
 package robot
+
+import (
+	"logger"
+	"rpc"
+)
+
+func (self *SRobot) HandlePongRsp(conn rpc.RpcConn, msg rpc.Pong) error {
+	logger.Info("recv HandlePongRsp : uid:%d, count:%d", self.uid, msg.Count)
+	self.stateMatchine.SwitchFsmState()
+	return nil
+}
