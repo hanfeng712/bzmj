9760155f877f5b6faa83c96eeb67cba7110f15fd
hanfeng <HF@8844028.com>
2018-08-09 00:31:45 -0700

    机器人配合状态机向大厅服务器发包

--- src/robot/robotRecvMsg.go
+++ src/robot/robotRecvMsg.go
@@ -8,7 +8,7 @@ import (
 func (self *SRobot) HandlePongRsp(conn rpc.RpcConn, msg rpc.Pong) error {
 	logger.Info("recv HandlePongRsp :")
 	//uid:%d, count:%d", self.uid, msg.Count)
-	//self.stateMatchine.SwitchFsmState()
+	self.stateMatchine.SwitchFsmState()
 	return nil
 }
 
