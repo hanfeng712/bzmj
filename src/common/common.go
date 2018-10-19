6a669e5fdf94983518ac799b39653384c0882e54
hanfeng <HF@8844028.com>
2018-10-18 22:55:46 -0700

    加入连接方式枚举

--- src/common/common.go
+++ src/common/common.go
@@ -4,3 +4,14 @@ type MsgQueueType struct {
 	//WebResult map[string]string
 	WebResult string
 }
+type Connect_Type struct {
+	WebSocket string
+	TcpSocket string
+	UdpSocket string
+}
+
+var ConnectType *Connect_Type = &Connect_Type{
+	WebSocket: "webSocket",
+	TcpSocket: "tcpSocket",
+	UdpSocket: "udpSocket",
+}
