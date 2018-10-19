6aa2c8713c4cac0a4190272256edb28ca0eb02c4
hanfeng <HF@8844028.com>
2018-10-19 01:42:02 -0700

    网络创建结构优化

--- src/common/common.go
+++ src/common/common.go
@@ -4,14 +4,3 @@ type MsgQueueType struct {
 	//WebResult map[string]string
 	WebResult string
 }
-type Connect_Type struct {
-	WebSocket string
-	TcpSocket string
-	UdpSocket string
-}
-
-var ConnectType *Connect_Type = &Connect_Type{
-	WebSocket: "webSocket",
-	TcpSocket: "tcpSocket",
-	UdpSocket: "udpSocket",
-}
